18f3121b1b1a7b872a3bda31265fc91a0485d504
salehloke <58175520+salehloke@users.noreply.github.com>
2022-10-01 13:07:10 +0800

    2 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  dynamicRowList;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    // init form array
+    this.dynamicRowList = this.fb.array([]);
+    const row1 = this.fb.group({
+      id: ['_'],
+      rowNo: [1],
+      first: ['saleh'],
+      last: ['loke'],
+      handle: ['@salehloke'],
+    });
+    this.dynamicRowList.push(row1);
   }
 
+  onRowDropped(element: any) {
+    console.log('dropped:', element);
+  }
 }
