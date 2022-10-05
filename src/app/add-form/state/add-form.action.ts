8524e05797516a32e4900b8d6271bacec372c525
salehloke <58175520+salehloke@users.noreply.github.com>
2022-10-05 10:51:40 +0800

    2 files modified

--- src/app/add-form/state/add-form.action.ts
+++ src/app/add-form/state/add-form.action.ts
@@ -1,11 +1,12 @@
 import { createAction, props } from '@ngrx/store';
+import { CurrencyPairing } from './add-form.reducer';
 
 export const addCurrencyPair = createAction(
   'Add Currency Pair',
-  props<{ content: string }>()
+  props<{ currencyFrom: string; currencyTo: string }>()
 );
 
 export const removeCurrencyPair = createAction(
   'Remove Currency Pair',
-  props<{ content: string }>()
+  props<{ id: string }>()
 );
--- src/app/add-form/state/add-form.reducer.ts
+++ src/app/add-form/state/add-form.reducer.ts
@@ -1,8 +1,9 @@
 import { createReducer } from '@ngrx/store';
+import { on } from '@ngrx/store/src/reducer_creator';
 import { addCurrencyPair, removeCurrencyPair } from './add-form.action';
 
 export interface AddCurrencyPairState {
-  currencyPairingList: [];
+  currencyPairingList: CurrencyPairing[];
   error: string;
   status: 'pending' | 'loading' | 'error' | 'success';
 }
@@ -13,15 +14,24 @@ export const initialState: AddCurrencyPairState = {
   status: 'pending',
 };
 
+export interface CurrencyPairing {
+  id: string;
+  currencyFrom: string;
+  currencyTo: string;
+}
+
 export const currencyPairReducer = createReducer(
   // supply initialState
   initialState,
-  on(addCurrencyPair, (state, { content }) => ({
+  on(addCurrencyPair, (state, { currencyFrom, currencyTo }) => ({
     ...state,
-    currencyPairingList: [state.currencyPairingList, { content: content }],
+    currencyPairingList: [
+      ...state.currencyPairingList,
+      { id: Date.now().toString(), currencyFrom, currencyTo },
+    ],
   })),
-  on(removeCurrencyPair, (state, { content }) => ({
+  on(removeCurrencyPair, (state, { id }) => ({
     ...state,
-    currencyPairingList: [state.currencyPairingList, { content: content }],
+    currencyPairingList: state.currencyPairingList.filter((pair) => pair.id),
   }))
 );
