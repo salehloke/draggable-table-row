8fc6a9d79611fe010b0dde063526081c9c7c46b0
salehloke <58175520+salehloke@users.noreply.github.com>
2022-10-10 12:55:34 +0800

    1 file modified

--- src/app/add-form/state/add-form.effects.ts
+++ src/app/add-form/state/add-form.effects.ts
@@ -1,32 +1,42 @@
-import { Injectable } from "@angular/core";
-import { Store } from "@ngrx/store";
-import { Actions, createEffect} from "@ngrx/effects";
-import { AppState } from "../../app.state";
-import { CurrencyPairsService } from "../currency-pairs.service";
-import { loadCurrencyPairs, loadCurrencyPairsFailure, loadCurrencyPairsSuccess } from "./add-form.action";
-import { switchMap, from, catchError,  } from "rxjs";
-
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Actions, createEffect, ofType, Effect } from '@ngrx/effects';
+import { AppState } from '../../app.state';
+import { CurrencyPairsService } from '../currency-pairs.service';
+import {
+  loadCurrencyPairs,
+  loadCurrencyPairsFailure,
+  loadCurrencyPairsSuccess,
+} from './add-form.action';
+import { switchMap, from, catchError, mergeMap } from 'rxjs';
 
 @Injectable()
-export class CurrencyPairsEffects{
+export class CurrencyPairsEffects {
   constructor(
     private actions$: Actions,
     private store: Store<AppState>,
     private currencyPairsService: CurrencyPairsService
-    ){
-  }
+  ) {}
 
+  loadcurrencyPairs$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadCurrencyPairs),
+      switchMap(() =>
+        // call the get Curerncy pairs method, convert it to an observable
+        from(this.currencyPairsService.getCurrencyPairs()).pipe(
+          map((currencyPairs) =>
+            loadCurrencyPairsSuccess({ currencyPairs: currencyPairs })
+          ),
+          catchError((error) => of(loadCurrencyPairsFailure))
+        )
+      )
+    )
+  );
 
-  loadcurrencyPairs$ = createEffect(()=>
-  this.actions$.pipe(
-    ofType(loadCurrencyPairs),
-    switchMap(()=>
-    // call the get Curerncy pairs method, convert it to an observable
-    from(this.currencyPairsService.getCurrencyPairs()).pipe(
-      map((currencyPairs)=> loadCurrencyPairsSuccess({currencyPairs: currencyPairs})),
-      catchError((error)=>of(loadCurrencyPairsFailure))
-    )
-    )
-  )
-  )
+  // @Effect()
+  // loadPairs$ = this.actions$
+  // .pipe(
+  //   ofType(loadCurrencyPairs),
+  //   mergeMap(()=>)
+  // )
 }
